a113007518326a3f594c97baa834f777cedd15a0
Benjamin Gutman <benjamingutman@icloud.com>
2024-12-09 17:15:26 -0500

    Added Private values to Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 using namespace std;
 
-#
+#ifndef DOCTOR_H
+#define DOCTOR_H
 
-class Doctor
+
+class Doctor{
+private:
+	string FirstName;
+	string LastName;
+	int id;
+	string Specialty;
+	int YearsOfExperience;
+	double BaseSalary;
+	double PerformanceBonus;
+public:
+
+
+};
