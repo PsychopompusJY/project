5935108b60f30fb97e859b50f3ab4fd5362e328e
Benjamin Gutman <benjamingutman@icloud.com>
2024-12-10 14:19:43 -0500

    Created Hospital.h and Patient.cpp

--- Hospital.h
+++ Hospital.h
@@ -22,8 +22,14 @@ private:
 	Doctor* D;
 	int NumP;
 	int NumD;
-
-
+public:
+	void Find_Oldest_Patient();
+	int Count_Critical_Patients();
+	void Doctors_By_Specialty(string);
+	void Show_Patient_By_ID(string);
+	void Show_Doctor_By_ID(string);
+	void Show_Assigned_Doctor(string);
+	void Show_Assigned_Patients(string);
 
 };
 
