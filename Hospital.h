9b392fa6ac3843dd6214b1f686427f6a5ee4b1b8
Benjamin Gutman <benjamingutman@icloud.com>
2024-12-10 09:50:59 -0500

    Created Hospital.h

---
+++ Hospital.h
@@ -0,0 +1,32 @@
+/*
+ * Hospital.h
+ *
+ *  Created on: Dec 10, 2024
+ *      Author: benjamin
+ */
+
+#include <iostream>
+#include <string>
+#include <array>
+#include "Doctor.h"
+#include "Patient.h"
+
+
+
+#ifndef HOSPITAL_H_
+#define HOSPITAL_H_
+
+class Hospital{
+private:
+	Patient* p;
+	Doctor* D;
+	int NumP;
+	int NumD;
+
+
+
+};
+
+
+
+#endif /* HOSPITAL_H_ */
