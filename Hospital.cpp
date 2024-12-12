409708567b945a22a14764bfe2b3c977948fd124
Benjamin Gutman <benjamingutman@icloud.com>
2024-12-12 15:16:52 -0500

    Created Hospital.cpp and added the includes

---
+++ Hospital.cpp
@@ -0,0 +1,15 @@
+/*
+ * Hospital.cpp
+ *
+ *  Created on: Dec 12, 2024
+ *      Author: benjamin
+ */
+
+
+#include <iostream>
+#include <string>
+#include <array>
+#include "Doctor.h"
+#include "Patient.h"
+#include "Hospital.h"
+
